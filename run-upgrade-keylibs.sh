ee643d0649a979f87ea245da17b9eb2597ae0a43
Gary Zhang <gzhang@primeobjects.com>
2022-01-23 11:24:07 -0800

    ^ libs

---
+++ run-upgrade-keylibs.sh
@@ -0,0 +1,6 @@
+sudo yarn upgrade react --latest
+sudo yarn upgrade twilio-sync --latest
+sudo yarn upgrade @types/react --latest
+sudo yarn upgrade @types/react-dom --latest
+sudo yarn upgrade axios --latest
+sudo yarn upgrade lodash --latest
